030a2229340108d29cdfa31ba26923d3cb4eb22e
CyberDay1 <126724028+CyberDay1@users.noreply.github.com>
2025-10-01 13:10:20 -0500

    Ensure Worldrise datapack auto-loads with updated worldgen

--- src/main/java/com/yourorg/worldrise/Worldrise.java
+++ src/main/java/com/yourorg/worldrise/Worldrise.java
@@ -2,10 +2,12 @@ package com.yourorg.worldrise;
 
 import com.cyberday1.theexpanse.MixinCompatBootstrap;
 import com.yourorg.worldrise.config.WorldriseConfig;
+import com.yourorg.worldrise.datapack.WorldriseDatapackInjector;
 import com.yourorg.worldrise.vendor.VendoredWorldgen;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +18,7 @@ public final class Worldrise {
     public Worldrise(ModContainer container) {
         MixinCompatBootstrap.enforce();
         VendoredWorldgen.init(container);
+        NeoForge.EVENT_BUS.addListener(WorldriseDatapackInjector::onServerAboutToStart);
         if (isMoonriseActive(WorldriseConfig.INSTANCE)) {
             LOGGER.info("Moonrise detected. Worldrise will avoid chunk pipeline interference.");
         }
