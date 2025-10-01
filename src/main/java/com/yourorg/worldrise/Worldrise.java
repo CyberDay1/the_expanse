a287b94af1eb8f7a5b1643eea5c4d63ba2e2b54d
CyberDay1 <126724028+CyberDay1@users.noreply.github.com>
2025-09-30 21:49:30 -0500

    Fix NeoForge config and bootstrap initialization

--- src/main/java/com/yourorg/worldrise/Worldrise.java
+++ src/main/java/com/yourorg/worldrise/Worldrise.java
@@ -2,12 +2,13 @@ package com.yourorg.worldrise;
 
 import com.cyberday1.theexpanse.MixinCompatBootstrap;
 import com.yourorg.worldrise.vendor.VendoredWorldgen;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod("worldrise")
 public final class Worldrise {
-    public Worldrise() {
+    public Worldrise(ModContainer container) {
         MixinCompatBootstrap.enforce();
-        VendoredWorldgen.init();
+        VendoredWorldgen.init(container);
     }
 }
