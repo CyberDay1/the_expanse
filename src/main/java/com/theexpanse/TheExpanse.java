09df66bd7615df0133ddbcff6c3127f44956cfaa
CyberDay1 <126724028+CyberDay1@users.noreply.github.com>
2025-10-03 13:16:10 -0500

    Fix carver configs and add density function dump

--- src/main/java/com/theexpanse/TheExpanse.java
+++ src/main/java/com/theexpanse/TheExpanse.java
@@ -1,5 +1,10 @@
 package com.theexpanse;
 
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.DensityFunction;
 import net.neoforged.fml.common.Mod;
 
 @Mod("the_expanse")
@@ -12,5 +17,11 @@ public class TheExpanse {
         // Ensure mixin bootstrap is run
         com.theexpanse.bootstrap.MixinCompatBootstrap.init();
         System.out.println("[TheExpanse] Mod constructor called");
+
+        RegistryAccess registryAccess = RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY);
+        Registry<DensityFunction> registry = registryAccess.registryOrThrow(Registries.DENSITY_FUNCTION);
+        registry.entrySet().forEach(e -> {
+            System.out.println("[TheExpanse][DF] " + e.getKey() + " -> " + e.getValue());
+        });
     }
 }
