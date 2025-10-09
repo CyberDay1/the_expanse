ba682c52b51d545dd56c248c9aec9622e6419aa9
CyberDay1 <126724028+CyberDay1@users.noreply.github.com>
2025-10-08 19:37:30 -0500

    chore: validate stonecutter builds

--- src/main/java/com/theexpanse/TheExpanse.java
+++ src/main/java/com/theexpanse/TheExpanse.java
@@ -2,10 +2,13 @@ package com.theexpanse;
 
 import net.neoforged.fml.common.Mod;
 
+import com.theexpanse.worldgen.OreScaler;
+
 @Mod(TheExpanse.MOD_ID)
 public final class TheExpanse {
     public static final String MOD_ID = "the_expanse";
 
     public TheExpanse() {
+        OreScaler.register();
     }
 }
