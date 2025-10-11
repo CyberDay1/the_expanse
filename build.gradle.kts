d137d83b390dd1150c67b7036d90f15f1d10ae1d
CyberDay1 <connormb5@gmail.com>
2025-10-11 16:51:55 -0500

    x

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("dev.kikugie.stonecutter") version "0.7.10"
     id("net.neoforged.gradle") version "6.0.18" apply false
     idea
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,14 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("dev.kikugie.stonecutter") version "0.7.10"
+}
+
 rootProject.name = "the_expanse"
+
+stonecutter {
+    vcs = "1.21.4"
+    defaultVersion = "1.21.1"
+    controller = "stonecutter.gradle.kts"
+}
